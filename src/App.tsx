5833fbf6fb4bdda521e0e3948c6ce0ad240daba6
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-03-12 18:29:33 -0600

    Add nextui framework to the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
+
+import { LineChart } from '@tremor/react';
+
+const chartdata = [
+  {
+    date: 'Jan 22',
+    SemiAnalysis: 2890,
+    'The Pragmatic Engineer': 2338,
+  },
+  {
+    date: 'Feb 22',
+    SemiAnalysis: 2756,
+    'The Pragmatic Engineer': 2103,
+  },
+  {
+    date: 'Mar 22',
+    SemiAnalysis: 3322,
+    'The Pragmatic Engineer': 2194,
+  },
+  {
+    date: 'Apr 22',
+    SemiAnalysis: 3470,
+    'The Pragmatic Engineer': 2108,
+  },
+  {
+    date: 'May 22',
+    SemiAnalysis: 3475,
+    'The Pragmatic Engineer': 1812,
+  },
+  {
+    date: 'Jun 22',
+    SemiAnalysis: 3129,
+    'The Pragmatic Engineer': 1726,
+  },
+  {
+    date: 'Jul 22',
+    SemiAnalysis: 3490,
+    'The Pragmatic Engineer': 1982,
+  },
+  {
+    date: 'Aug 22',
+    SemiAnalysis: 2903,
+    'The Pragmatic Engineer': 2012,
+  },
+  {
+    date: 'Sep 22',
+    SemiAnalysis: 2643,
+    'The Pragmatic Engineer': 2342,
+  },
+  {
+    date: 'Oct 22',
+    SemiAnalysis: 2837,
+    'The Pragmatic Engineer': 2473,
+  },
+  {
+    date: 'Nov 22',
+    SemiAnalysis: 2954,
+    'The Pragmatic Engineer': 3848,
+  },
+  {
+    date: 'Dec 22',
+    SemiAnalysis: 3239,
+    'The Pragmatic Engineer': 3736,
+  },
+];
+
+const dataFormatter = (number: number) =>
+  `$${Intl.NumberFormat('us').format(number).toString()}`;
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        Jammming
-      </div>
+      <LineChart
+        className="h-80"
+        data={chartdata}
+        index="date"
+        categories={['SemiAnalysis', 'The Pragmatic Engineer']}
+        colors={['indigo', 'rose']}
+        valueFormatter={dataFormatter}
+        yAxisWidth={60}
+        onValueChange={(v) => console.log(v)}
+      />
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            alt="nextui logo"
+            height={40}
+            radius="sm"
+            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+            width={40}
+          />
+          <div className="flex flex-col">
+            <p className="text-md">NextUI</p>
+            <p className="text-small text-default-500">nextui.org</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Link
+            isExternal
+            showAnchorIcon
+            href="https://github.com/nextui-org/nextui"
+          >
+            Visit source code on GitHub.
+          </Link>
+        </CardFooter>
+      </Card>
     </>
-  )
+  );
 }
 
 export default App
